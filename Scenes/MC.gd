9b05312d038b1d1f1c77b0dd5efb27973dcc41c3
Matthew Paes <104486473+ST10042612@users.noreply.github.com>
2024-09-24 15:38:21 +0200

    Added projectiles to the game

--- Scenes/MC.gd
+++ Scenes/MC.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 
-#var slash(slash)
-var slash_Scene = preload("res://Scenes/slash.tscn")
+var SLASH = (preload("res://Scenes/slash.tscn"))
 
 func _process(delta):
 	look_at(get_global_mouse_position())
+
+func shoot():
+	var slash = SLASH.instantiate()
+	get_parent().add_child(slash)
+	slash.position = $Marker2D.global_position
+	slash.rotation = $Marker2D.global_rotation - 80
+	slash.velocity = get_global_mouse_position() - slash.position
 
 func _physics_process(delta):
 	
@@ -25,5 +31,8 @@ func _physics_process(delta):
 		velocity = Vector2.ZERO
 	if Input.is_action_just_released("right"):
 		velocity = Vector2.ZERO			
+	
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 	
 	move_and_slide()
--- Scenes/slash.gd
+++ Scenes/slash.gd
@@ -1,13 +1,13 @@
-extends Area2D
+extends CharacterBody2D
 
-var speed = 300
-
-var movement_vector = Vector2(0,-1)
+var speed = 250
+#var vel = Vector2(0 , 0)
 
 func _physics_process(delta):
-	global_position  += movement_vector.rotated(rotation) * speed * delta
-
+	var collision_info = move_and_collide((velocity.normalized()) * delta * speed)
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
-	print("die bitch") # Replace with function body.
+	print("ded") # Replace with function body.
+
+
